d722fc3a4694b6cf7f7dddecb789647d50e70434
Shashwat Jaiswal <shashwatjaiswal2001@gmail.com>
2022-04-26 15:29:49 +0530

    fixed scripts

--- redirect/update.sh
+++ redirect/update.sh
@@ -1,6 +1,4 @@
-set GOOS=linux
-
-go build -o redirect main.go
-build-lambda-zip -o deployment.zip redirect
+GOOS=linux GOARCH=amd64 go build -o redirect main.go
+zip deployment.zip redirect
 
 aws lambda update-function-code --function-name RedirectFunction --region ap-south-1 --zip-file fileb://./deployment.zip
